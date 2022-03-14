7a9ece6abd0301dd059f26275552fcad4151d4ad
Marco <marcobarbon97@gmail.com>
2022-03-14 15:16:16 +0100

    aggiunta struttura ROS al nodo agevar

--- scripts/constant.py
+++ scripts/constant.py
@@ -4,7 +4,7 @@ N_MOD = 3 #Number of modules of the robot
 
 ANGLE_MAX = 35 #Is the maximum angle between two moduless
 
-FREQ = 50 #[Herzt] Is the refresh rate of the program. Is you to determinate the time passed from the previous operation
+FREQ = 0.5 #[Herzt] Is the refresh rate of the program. Is you to determinate the time passed from the previous operation
 
 DIST_MOD = 8 #Cm. Da cambiare, ne ho usato uno a caso perché non so quanto sia il dato reale
 
