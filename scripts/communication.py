687c360cc612720982fb3f0e664e66a92366b4b9
Andrea Grillo <grilloandrea6@gmail.com>
2022-03-28 12:20:01 +0200

    working on communication

--- scripts/communication.py
+++ scripts/communication.py
@@ -2,15 +2,30 @@
 
 import rospy
 from ReseQROS.msg import Motor
+import smbus as smbus
+
+
+def writeNumbers(addr,values):
+    byteList = []
+    for value in values:
+        byteList += list(struct.pack('f', value))
+    byteList.append(0)  # fails to send last byte over I2C, hence this needs to be added 
+    bus.write_i2c_block_data(addr, byteList[0], byteList[1:12])
+
+
 
 def invio_dati(data):
 	rospy.loginfo("DataToSend:\nADDR: " + str(data.address) + "\nVSX: " + str(data.vsx) + "\nVDX: " + str(data.vdx) + "\nANGLE: " + str(data.angle))
+	#ToDo convert data to set of bytes
+	values = [float(data.vsx),float(data.vdx),float(data.angle)]
+	writeNumbers(data.address,values)
 
 
 if __name__ == '__main__':
 	try:
+		I2Cbus = smbus.SMBus(1)
+
 		rospy.init_node('communication')
-		rate = rospy.Rate(50) #frecuency in hertz - should be the same as agevar node (or more(?)) 
 
 		rospy.loginfo("Hello! communication node started!")
 
