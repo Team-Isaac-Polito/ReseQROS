8bee3c75782fbd517acced9f44761603eafe553f
MAarco10 <marcobarbon97@gmail.com>
2022-06-08 16:49:06 +0200

    creata la struttura ROS per agevar_v3 e riorganizzata la cartella script

--- scripts/agevar_v2/constant.py
+++ scripts/agevar_v2/constant.py
@@ -1,10 +1,12 @@
 # Constant
 
-N_MOD = 1           #Number of modules of the robot
+N_MOD = 1           # Number of modules of the robot
+
+N_VAR = 7           # Number of kinetic variables
 
 ANGLE_MAX = 35      # Is the maximum angle between two moduless
 
-FREQ = 0.5          # [Herzt] Is the refresh rate of the program. Is you to determinate the time passed from the previous operation
+FREQ = 50          # [Herzt] Is the refresh rate of the program. Is you to determinate the time passed from the previous operation
 
 DIST_MOD = 8        # Cm. Da cambiare, ne ho usato uno a caso perché non so quanto sia il dato reale
 
@@ -12,8 +14,13 @@ FILE_PATH = "/home/andrea/agevarFiles/modulo_" # Percorso in cui verranno salvar
 
 ADDRESSES = [ 21, 48, 69 ] # 0x15, 0x30, 0x45
 
-JOYSTICK_FREQ = 20  # Frequenza con cui acquisiamo i dati dal joystick
+#JOYSTICK_FREQ = 20  # Frequenza con cui acquisiamo i dati dal joystick
 
-a = 10              # Dimensione dell'interasse del modulo
 
-b = 10              # 
+a = 1         # [m]
+
+b = 1         # [m]     
+
+d = 0.2         # [m]
+
+r = 0.05        # [m]
