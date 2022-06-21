93074f26eb871544a04f5af19235960a20577bd3
Marco Fincato <marcofincato777@gmail.com>
2022-06-22 01:46:21 +0200

    Fixed CANbus
    Corrected indentation, removed system calls, code compliant to latest python-can version, avoid sending void bytes, removed unnecessary global and import implemented test loop

--- scripts/communication/communication.py
+++ scripts/communication/communication.py
@@ -1,37 +1,28 @@
+#!/usr/bin/python
+
 import definitions
 import can
-import time
 import os
 
 import rospy
 from ReseQROS.msg import Motor
 
-print('\n\rCAN Rx test')
-print('Bring up CAN0....')
-
-
 
 def writeNumbers(addr,vsx,vdx,angle):
-    global canbus
+	a = vsx & 0xFF
+	b = vsx>>8 & 0xFF
+	msg = can.Message(arbitration_id=addr,data=[definitions.DATA_TRACTION_LEFT, a, b],is_extended_id=False)
+	canbus.send(msg)
 
-    a = vsx & 0xFF
-    b = vsx>>8 & 0xFF
-	msg = can.Message(arbitration_id=addr,data=[definitions.DATA_TRACTION_LEFT,a,b, 0x0, 0x0, 0x0,0x0, 0x0],extended_id=False)
-    canbus.send(msg)
+	a = vdx & 0xFF
+	b = vdx>>8 & 0xFF
+	msg = can.Message(arbitration_id=addr,data=[definitions.DATA_TRACTION_RIGHT, a, b],is_extended_id=False)
+	canbus.send(msg)
 
-    a = vdx & 0xFF
-    b = vdx>>8 & 0xFF
-	msg = can.Message(arbitration_id=addr,data=[definitions.DATA_TRACTION_RIGHT, a, b, 0x0, 0x0, 0x0,0x0, 0x0],extended_id=False)
-    canbus.send(msg)
-
-    a = angle & 0xFF
-    b = angle>>8 & 0xFF
-	msg = can.Message(arbitration_id=addr,data=[definitions.DATA_TRACTION_YAW, a, b, 0x0, 0x0, 0x0,0x0, 0x0],extended_id=False)
-    canbus.send(msg)
-
-
-
-
+	a = angle & 0xFF
+	b = angle>>8 & 0xFF
+	msg = can.Message(arbitration_id=addr,data=[definitions.DATA_YAW, a, b],is_extended_id=False)
+	canbus.send(msg)
 
 
 def invio_dati(data):
@@ -39,25 +30,29 @@ def invio_dati(data):
 	writeNumbers(data.address,int(data.vsx),int(data.vdx),int(data.angle))
 
 
+def testloop():
+	vsx = 512
+	vdx = 512
+	yaw = 512
+	rate = rospy.Rate(5) # 5 Hz
+	while 1:
+		writeNumbers(21, vsx, vdx, yaw)
+		writeNumbers(22, vsx, vdx, yaw)
+		writeNumbers(23, vsx, vdx, yaw)
+		rate.sleep()
+
+
 if __name__ == '__main__':
-    global canbus
 	try:
-
 		rospy.init_node('communication')
-
 		rospy.loginfo("Hello! communication node started!")
 
-        # Bring up can0 interface at 125kbps
-        os.system("sudo modprobe can")
-        os.system("sudo /sbin/ip link set can0 up type can bitrate 125000")
-        time.sleep(0.1)	
-        canbus = can.interface.Bus(channel='can0', bustype='socketcan_native')
-
+		canbus = can.interface.Bus(channel='can0', bustype='socketcan')
 
 		rospy.Subscriber("motor_topic",Motor,invio_dati)
 
 		rospy.spin()
-		
+
 
 	except rospy.ROSInterruptException:
 		pass
