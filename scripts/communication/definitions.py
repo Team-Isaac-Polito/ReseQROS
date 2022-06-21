319a929662e6437872c98114cdb036493b73d0b9
Andrea Grillo <grilloandrea6@gmail.com>
2022-06-21 19:14:43 +0200

    added canbus test files and communicationdirectory

---
+++ scripts/communication/definitions.py
@@ -0,0 +1,12 @@
+
+
+DATA_TRACTION_LEFT =           0x01
+DATA_TRACTION_RIGHT =          0x02
+DATA_YAW =                     0x03
+DATA_PITCH =                   0x04
+SEND_STATUS =                  0x05                
+SEND_IMU_DATA =                0x06
+SEND_YAW_ENCODER =             0x07    
+SEND_TRACTION_LEFT_SPEED =     0x08 
+SEND_TRACTION_RIGHT_SPEED =    0x09            
+SEND_BATTERY_VOLTAGE =         0x0A        
