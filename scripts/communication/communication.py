e11a0f170822a5c95dda6809e78a74b47b3226a8
grilloandrea6 <grilloandrea6@gmail.com>
2022-06-23 18:58:58 +0200

    testRemoteControl

--- scripts/communication/communication.py
+++ scripts/communication/communication.py
@@ -3,10 +3,13 @@
 import definitions
 import can
 import os
-
+import time
 import rospy
 from ReseQROS.msg import Motor
 
+vel=0
+curv=0
+tempo = 0
 
 def writeNumbers(addr,vsx,vdx,angle):
 	a = vsx & 0xFF
@@ -25,10 +28,29 @@ def writeNumbers(addr,vsx,vdx,angle):
 	canbus.send(msg)
 
 
-def invio_dati(data):
+def invio_dati():
 	rospy.loginfo("DataToSend:\nADDR: " + str(data.address) + "\nVSX: " + str(data.vsx) + "\nVDX: " + str(data.vdx) + "\nANGLE: " + str(data.angle))
-	writeNumbers(data.address,int(data.vsx),int(data.vdx),int(data.angle))
+	global vel
+	global curv
+	rospy.loginfo("executing writenumbers - vel " + str(vel) + " - curv - " + str(curv))
 
+	# filtro posizioe nulla
+	velo = 512 if 462 < vel < 562 else vel
+	curvo = 512 if 462 < curv < 562 else curv
+
+	velo = velo - 512		#-512 +512
+	curvo = curvo - 512		#-512 +512
+
+	vsx,vdx = mappaDrive(velo,curvo)
+
+	writeNumbers(21,int(vsx),int(vdx),int(0))
+	writeNumbers(22,int(vsx),int(vdx),int(0))
+	writeNumbers(23,int(vsx),int(vdx),int(0))
+
+def mappaDrive(velo,curvo):
+	vsx = velo + curvo/2
+	vdx = velo - curvo/2
+	return vsx,vdx
 
 def testloop():
 	vsx = 512
@@ -42,6 +64,25 @@ def testloop():
 		rate.sleep()
 
 
+
+def vel_list(dataa):
+	global vel
+	global tempo
+	vel = int(dataa.data)
+#	rospy.loginfo("received vel")
+#	rospy.loginfo("--vel " + str(vel) + "- curv " + str(curv))
+	if time.time() - tempo > 0.1:
+		tempo = time.time()
+		invio_dati()
+
+
+def curv_list(dataa):
+	global curv
+	global tempo
+	curv = int(dataa.data)
+	#rospy.loginfo("received curv")
+
+
 if __name__ == '__main__':
 	try:
 		rospy.init_node('communication')
@@ -49,7 +90,9 @@ if __name__ == '__main__':
 
 		canbus = can.interface.Bus(channel='can0', bustype='socketcan')
 
-		rospy.Subscriber("motor_topic",Motor,invio_dati)
+		#rospy.Subscriber("motor_topic",Motor,invio_dati)
+		rospy.Subscriber("vel",UInt16,vel_list)
+		rospy.Subscriber("curv",UInt16,curv_list)
 
 		rospy.spin()
 
