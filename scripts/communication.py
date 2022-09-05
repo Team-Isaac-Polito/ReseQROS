a0b0e4cc30dd1ba303b705faf7582e56f62102fe
grilloandrea6 <grilloandrea6@gmail.com>
2022-09-05 11:07:25 +0200

    cleaned up communication.py

--- scripts/communication.py
+++ scripts/communication.py
@@ -7,9 +7,6 @@ import time
 import rospy
 from ReseQROS.msg import Motor
 from std_msgs.msg import UInt16
-vel=0
-curv=0
-tempo = 0
 
 def writeNumbers(addr,vsx,vdx,angle):
 	out = vsx.to_bytes(2, byteorder='little', signed=True)
@@ -24,25 +21,9 @@ def writeNumbers(addr,vsx,vdx,angle):
 	msg = can.Message(arbitration_id=addr,data=[definitions.DATA_YAW, out[0], out[1]],is_extended_id=False)
 	canbus.send(msg)
 
-
-
-
-
-#def vel_list(dataa):
-#	global vel
-#	global tempo
-#	vel = int(dataa.data)
-#	rospy.loginfo("received vel")
-#	rospy.loginfo("--vel " + str(vel) + "- curv " + str(curv))
-#	if time.time() - tempo > 0.1:
-#		tempo = time.time()
-#		invio_dati()
-
-
 def motor_list(dataa):
 	writeNumbers(int(dataa.address),int(dataa.wsx),int(dataa.wdx),int(dataa.angle))
 	#rospy.loginfo("received curv")
-
 
 if __name__ == '__main__':
 	try:
@@ -52,11 +33,8 @@ if __name__ == '__main__':
 		canbus = can.interface.Bus(channel='can0', bustype='socketcan')
 
 		rospy.Subscriber("motor_topic",Motor,motor_list)
-		#rospy.Subscriber("vel",UInt16,vel_list)
-		#rospy.Subscriber("curv",UInt16,curv_list)
 
 		rospy.spin()
-
 
 	except rospy.ROSInterruptException:
 		pass
