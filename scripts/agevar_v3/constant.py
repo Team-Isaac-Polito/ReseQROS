f44a2b8b485f4f6333da776c14467abf4acd0aac
Marco Barbon <marcobarbon97@gmail.com>
2022-09-07 17:05:12 +0200

    piccola modifica

--- scripts/agevar_v3/constant.py
+++ scripts/agevar_v3/constant.py
@@ -21,7 +21,7 @@ r = 0.05        # [m] raggio delle ruote equivalenti
 
 w_max = 2*pi # [rad/s] massima velocità di rotazione della ruota equivalente (motore di avanzamento)
 
-Max_Lin_Vel= (2*pi*w_max*r)/2 # [m/s] massima velocità lineare di un modulo
+Max_Lin_Vel= (w_max*r)/2 # [m/s] massima velocità lineare di un modulo
 
 Min_Curv = 3*d # [m] raggio minimo di curvatura  
 Max_Curv = 10*Min_Curv # [m] raggio massimo di curvatur
