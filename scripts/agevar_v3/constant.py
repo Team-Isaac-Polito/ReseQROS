2a87f985183ff71786fe18111e6b255a6ad9237c
Marco Barbon <marcobarbon97@gmail.com>
2022-09-07 15:06:48 +0200

    rivisto constant agevar

--- scripts/agevar_v3/constant.py
+++ scripts/agevar_v3/constant.py
@@ -4,27 +4,24 @@ from math import cos, sin, radians, pi
 
 N_MOD = 3          # Number of modules of the robot
 
-#serve ancora? TODO
-N_VAR = 7           # Number of kinetic variables 
-
 ANGLE_MAX = 35      # [°] Is the maximum angle between two moduless
 
 FREQ = 50          # [Hz] Is the refresh rate of the program. Is you to determinate the time passed from the previous operation
 Ts = 1/FREQ        # [s] Periodo di iterazione dell'algoritmo, da scegliere e implementare con ROS
 
-ADDRESSES = [ 21, 22, 23 ] # 0x15, 0x30, 0x45
+ADDRESSES = [ 23, 22, 21 ] # 0x17, 0x16, 0x15
 
-a = 0.15        # [m] distanza tra il punto centrale del modulo i-esimo e il giunto d'imbardata precedente
+a = 0.18        # [m] distanza tra il punto centrale del modulo i-esimo e il giunto d'imbardata precedente
 
-b = 0.15        # [m] distanza tra il punto centrale del modulo i-esimo e il giunto d'imbardata successivo
+b = 0.18        # [m] distanza tra il punto centrale del modulo i-esimo e il giunto d'imbardata successivo
 
-d = 0.18         # [m] lunghezza dell'interasse tra le due ruote equivalenti
+d = 0.21         # [m] lunghezza dell'interasse tra le due ruote equivalenti
 
 r = 0.05        # [m] raggio delle ruote equivalenti
 
-w_max = 5 # [Hz] massima velocità di rotazione di un motore di avanzamento
+w_max = 1 # [Hz] massima velocità di rotazione della ruota equivalente (motore di avanzamento)
 
-Max_Lin_Vel= w_max*r/2 # [m/s] massima velocità lineare di un modulo
+Max_Lin_Vel= (2*pi*w_max*r)/2 # [m/s] massima velocità lineare di un modulo
 
-Min_Curv = 3*d # [m] valori presi 
-Max_Curv = 10*Min_Curv # [m]
+Min_Curv = 3*d # [m] raggio minimo di curvatura  
+Max_Curv = 10*Min_Curv # [m] raggio massimo di curvatur
