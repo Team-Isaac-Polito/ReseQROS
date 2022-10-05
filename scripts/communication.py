b10d7b3369029f70fa4681163fe96b6580a63353
Andrea Grillo <grilloandrea6@gmail.com>
2022-10-05 16:02:24 +0200

    communication listen to twist and not uint16

--- scripts/communication.py
+++ scripts/communication.py
@@ -7,6 +7,7 @@ import time
 import rospy
 from ReseQROS.msg import Motor
 from std_msgs.msg import UInt16
+from geometry_msgs.msg import Twist
 
 
 eex_val = 512
@@ -14,14 +15,10 @@ eey_val = 1023
 eez_val = 512
 pitch_val = 0 # ToDo check starting Value
 
-eex_time = eey_time = eez_time = pitch_time = time.time()
+last_time = time.time()
 
-
-def filter_center(data):
-    return 512 if 462 < data < 562 else data
 def interval(data):
     return data if 0 <= data <= 1023 else 1023 if data > 1023 else 0 
-
 
 def motor_list(dataa):
     out = int(dataa.wsx).to_bytes(2, byteorder='little', signed=True)
@@ -36,79 +33,48 @@ def motor_list(dataa):
     msg = can.Message(arbitration_id=int(dataa.address),data=[definitions.DATA_YAW, out[0], out[1]],is_extended_id=False)
     canbus.send(msg)
 
-
-def pitch_list(dataa):
+def twist_list(data):
     global pitch_val
-    global pitch_time
+    global eex_val
+    global eey_val
+    global eez_val
 
     act_time = time.time()
-    dataa.data = filter_center(dataa.data)
-    pitch_val += ((dataa.data-512) / 6000) * (act_time - eey_time)
+    dt = act_time - last_time
+    
+    #pitch
+    pitch_val += data.linear.z * dt
     pitch_val = interval(pitch_val)
+    
+    #EE_pitch
+    eey_val += data.angular.y * dt
+    eey_val = interval(eey_val)
+
+    #EE_pitch2
+    eez_val += (data.angular.y + data.angular.z) * dt
+    eez_val = interval(eez_val)
+
+    #EE_roll
+    eex_val += data.angular.x * dt
+    eex_val = interval(eex_val)
+
+    last_time = act_time
+
     out = int(pitch_val).to_bytes(2, byteorder='little', signed=True)
-    pitch_time = act_time
-    # ToDo at the moment address is hardcoded
     msg = can.Message(arbitration_id=0x17,data=[definitions.DATA_PITCH, out[0], out[1]],is_extended_id=False) 
     canbus.send(msg)
 
-
-def ee_pitch_list(dataa):
-    global eey_val
-    global eey_time
-    global eez_val
-    global eez_time
-
-    act_time = time.time()
-    dataa.data = filter_center(dataa.data)
-    eey_val += (dataa.data-512) * (act_time - eey_time)
-    eey_val = interval(eey_val)
-    eey_time = act_time
-
     out = int(eey_val).to_bytes(2, byteorder='little', signed=True)
-    # ToDo at the moment address is hardcoded
     msg = can.Message(arbitration_id=0x15,data=[definitions.DATA_EE_PITCH, out[0], out[1]],is_extended_id=False) 
     canbus.send(msg)
 
-    act_time = time.time()
-    eez_val += (filter_center(dataa.data)-512) * (act_time - eez_time)
-    eez_val = interval(eez_val)
-    eez_time = act_time
-
     out = int(eez_val).to_bytes(2, byteorder='little', signed=True)
-    # ToDo at the moment address is hardcoded
     msg = can.Message(arbitration_id=0x15,data=[definitions.DATA_EE_PITCH2, out[0], out[1]],is_extended_id=False) 
     canbus.send(msg)
-
-
-def ee_pitch2_list(dataa):
-    global eez_val
-    global eez_time
-
-    act_time = time.time()
-    eez_val += (filter_center(dataa.data)-512) * (act_time - eez_time)
-    eez_val = interval(eez_val)
-    eez_time = act_time
-
-    out = int(eez_val).to_bytes(2, byteorder='little', signed=True)
-    # ToDo at the moment address is hardcoded
-    msg = can.Message(arbitration_id=0x15,data=[definitions.DATA_EE_PITCH2, out[0], out[1]],is_extended_id=False) 
-    canbus.send(msg)
-
-
-def ee_roll_list(dataa):
-    global eex_val
-    global eex_time
-
-    act_time = time.time()
-    eex_val += (filter_center(dataa.data)-512) * (act_time - eex_time)
-    eex_val = interval(eex_val)
-    eex_time = act_time
 
     out = int(eex_val).to_bytes(2, byteorder='little', signed=True)
-    # ToDo at the moment address is hardcoded
     msg = can.Message(arbitration_id=0x15,data=[definitions.DATA_EE_ROLL, out[0], out[1]],is_extended_id=False) 
     canbus.send(msg)
-
 
 if __name__ == '__main__':
     try:
@@ -118,10 +84,7 @@ if __name__ == '__main__':
         canbus = can.interface.Bus(channel='can0', bustype='socketcan')
 
         rospy.Subscriber("motor_topic",Motor,motor_list)
-        rospy.Subscriber("eex",UInt16,ee_roll_list)
-        rospy.Subscriber("eey",UInt16,ee_pitch_list)
-        rospy.Subscriber("eez",UInt16,ee_pitch2_list)
-        rospy.Subscriber("pitch",UInt16,pitch_list)
+        rospy.Subscriber("twist_joystick",Twist,twist_list)
 
         rospy.spin()
 
