043b6c25215d48ddbab011351c03d0800d8a0384
Team Isaac Polito <team.isaac.polito@gmail.com>
2022-10-06 13:11:13 +0200

    test code

---
+++ scripts/commands.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import definitions
+import can
+import os
+import time
+import rospy
+from ReseQROS.msg import Motor
+from std_msgs.msg import UInt16
+from geometry_msgs.msg import Twist
+
+canbus = can.interface.Bus(channel='can0', bustype='socketcan')
+
+def motor_list(addr,sx,dx):
+    out = int(sx).to_bytes(2, byteorder='little', signed=True)
+    msg = can.Message(arbitration_id=int(addr),data=[definitions.DATA_TRACTION_LEFT, out[0], out[1]],is_extended_id=False)
+    canbus.send(msg)
+
+    out = int(dx).to_bytes(2, byteorder='little', signed=True)
+    msg = can.Message(arbitration_id=int(addr),data=[definitions.DATA_TRACTION_RIGHT, out[0], out[1]],is_extended_id=False)
+    canbus.send(msg)
+
+    #out = int(dataa.angle).to_bytes(2, byteorder='little', signed=True)
+    #msg = can.Message(arbitration_id=int(dataa.address),data=[definitions.DATA_YAW, out[0], out[1]],is_extended_id=False)
+    #canbus.send(msg)
+while 1:
+    motor_list(0x15,300,300)
+    #motor_list(0x16,300,300)
+    #motor_list(0x17,300,300)
+    time.sleep(0.1)
