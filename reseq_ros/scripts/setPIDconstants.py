abde5208ad1042272acbab52ffa5d94cfb3d6faa
Andrea Grillo <grilloandrea6@gmail.com>
2022-12-16 17:27:37 +0100

    setPidConstants fixed

--- reseq_ros/scripts/setPIDconstants.py
+++ reseq_ros/scripts/setPIDconstants.py
@@ -2,19 +2,22 @@
 
 import definitions
 import can
+import sys
+import struct
 
-canbus = can.interface.Bus(channel='can0', bustype='socketcan')
+addr = 0x15
+
+canbus = can.interface.Bus(channel='can1', bustype='socketcan')
 kp,ki,kd = sys.argv[1:]
 
-data = list(struct.pack('f', kp))
+data = list(struct.pack('f', float(kp)))
 msg = can.Message(arbitration_id=int(addr),data=[definitions.DATA_PID_KP]+data,is_extended_id=False)
 canbus.send(msg)
 
-data = list(struct.pack('f', ki))
+data = list(struct.pack('f', float(ki)))
 msg = can.Message(arbitration_id=int(addr),data=[definitions.DATA_PID_KI]+data,is_extended_id=False)
 canbus.send(msg)
 
-data = list(struct.pack('f', kd))
+data = list(struct.pack('f', float(kd)))
 msg = can.Message(arbitration_id=int(addr),data=[definitions.DATA_PID_KD]+data,is_extended_id=False)
 canbus.send(msg)
-
