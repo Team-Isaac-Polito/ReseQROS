ad0340187e8a171c426dfa7634b8296d9affe076
Marco Barbon <marcobarbon97@gmail.com>
2023-02-08 18:32:52 +0100

    new agevar_core node

---
+++ reseq_ros/scripts/agevar/agevar_core.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+import rospy
+
+from reseq_ros.msg import Real_input
+from reseq_ros.msg import Real_output
+
+from agevar_constant import *
+from agevar_kinematic import *
+
+'''
+@Marco Barbon [287462]
+For further details see Agevar's Report
+'''
+
+"""Global constants"""
+delta = [0]*N_mod
+
+"""ROS structure"""
+
+# Computes and publishes the rotation speed of the feed motors (wdx,wsx) and the yaw angle (delta) of every module
+def callback(dataa):
+    global delta
+
+    # lin_vel, ang_vel and sign of the first module
+    lin_vel = dataa.lin_vel
+    ang_vel = dataa.ang_vel
+    sign = dataa.sign
+
+    # definition of ROS publishers
+    pub = [rospy.Publisher("Real_output_module_"+str(i),Real_output,queue_size=10) for i in range(N_mod)]
+    Real_output_msg = Real_output()
+
+    # module vector
+    if sign==1:
+        module_vector= list(range(N_mod)) # forward
+    else:
+        module_vector= list(range(N_mod)).reverse() # backward
+
+    # for every module...
+    for num_module in module_vector:
+
+        Real_output_msg.num_module = num_module
+        Real_output_msg.sign = sign
+        Real_output_msg.lin_vel = lin_vel
+        Real_output_msg.ang_vel = ang_vel
+        Real_output_msg.delta = delta[num_module]
+        pub[num_module].publish(Real_output_msg)
+
+        if num_module != module_vector[-1]: # for every module except the last one
+            # kinematic calculations
+            # for more details see agevar_kinematic.py
+            lin_vel,ang_vel = agevar_kinematic(lin_vel,ang_vel,delta,num_module,sign) 
+
+# Receives the values of feed speed and curve radius of the first module from the remote controller
+# on the topic Real_input every time new values are available and it uses them to run the callback
+def listener():
+    rospy.Subscriber("Real_input",Real_input,callback)
+    rospy.spin()
+
+# Main function 
+def main_function():
+    rospy.init_node('agevar_core')
+    rospy.loginfo("Hello! agevar_core node started!") 
+    listener() 
+
+if __name__ == '__main__':
+	try:
+		main_function()
+	except rospy.ROSInterruptException:
+		pass
--- reseq_ros/scripts/agevar/remote_test.py
+++ reseq_ros/scripts/agevar/remote_test.py
@@ -12,27 +12,38 @@ def publisher():
     rate=rospy.Rate(freq)
 
     t=0
-    t_sim=5
+    t_sim=10
     t_step=t_sim/4
 
-    while not rospy.is_shutdown() and t<t_sim:
+    while not rospy.is_shutdown():
+
+        if t >= t_sim:
+            t = 0
 
         if t<=t_step:
-            print('dritto')
-            lin_vel=0 #512-int(512/t_step*t) # linear acceleration
-            r_curv=512
+            # print('dritto')
+            if t<=t_step/2:
+                lin_vel=512-int(512/t_step*2*t) # linear acceleration
+                r_curv=512
+            else:
+                lin_vel=0 # constant velocity
+                r_curv=512
         elif t_step<t<=2*t_step:
-            print('curva 1')
+            # print('curva 1')
             lin_vel=0
             r_curv=1023
         elif 2*t_step<t<3*t_step:
-            print('curva 2')
+            # print('curva 2')
             lin_vel=0
             r_curv=0
         else:
-            print('fermo')
-            lin_vel=512
-            r_curv=512
+            # print('fermo')
+            if t<=7*t_step/2:
+                lin_vel=int(512/t_step*2*(t-3*t_step)) # linear deceleration
+                r_curv=512
+            else:
+                lin_vel=512 # rest
+                r_curv=512
 
         twist_msg.linear.y = lin_vel
         twist_msg.linear.x = r_curv
