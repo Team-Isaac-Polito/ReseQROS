ebb8c3aaac7565bbda938d31f6a51ca258626597
292801 <118939868+292801@users.noreply.github.com>
2023-02-24 14:52:08 +0100

    Module yaw rate correction
    Implemented correction of module yaw rate according to measured yaw angle errore

--- reseq_ros/scripts/agevar/agevar_core.py
+++ reseq_ros/scripts/agevar/agevar_core.py
@@ -4,6 +4,7 @@ import rospy
 
 from reseq_ros.msg import Real_input
 from reseq_ros.msg import Real_output
+from std_msgs.msg import Float32
 
 from agevar_constant import *
 from agevar_kinematic import *
@@ -15,17 +16,20 @@ For further details see Agevar's Report
 
 """Global constants"""
 delta = [0]*N_mod
+yaw_angle_middle = 0
+yaw_angle_tail = 0
 
 """ROS structure"""
 
 # Computes and publishes the rotation speed of the feed motors (wdx,wsx) and the yaw angle (delta) of every module
 def callback(dataa):
-    global delta
+    global delta, yaw_angle_middle, yaw_angle_tail
 
     # lin_vel, ang_vel and sign of the first module
     lin_vel = dataa.lin_vel
     ang_vel = dataa.ang_vel
     sign = dataa.sign
+    delta_meas = [yaw_angle_middle,yaw_angle_tail]
 
     # definition of ROS publishers
     pub = [rospy.Publisher("real_output_"+str(i),Real_output,queue_size=10) for i in range(N_mod)]
@@ -50,12 +54,18 @@ def callback(dataa):
         if num_module != module_vector[-1]: # for every module except the last one
             # kinematic calculations
             # for more details see agevar_kinematic.py
-            lin_vel,ang_vel = agevar_kinematic(lin_vel,ang_vel,delta,num_module,sign) 
+            lin_vel,ang_vel = agevar_kinematic(lin_vel,ang_vel,delta,delta_meas,num_module,sign)
+
+def callback_float(dataa,var):
+    globals()[var] = dataa.data      
 
 # Receives the values of feed speed and curve radius of the first module from the remote controller
 # on the topic Real_input every time new values are available and it uses them to run the callback
 def listener():
-    rospy.Subscriber("real_input",Real_input,callback)
+    rospy.Subscriber("yaw_angle_middle",Float32,callback_float,"yaw_angle_middle")
+    rospy.Subscriber("yaw_angle_tail",Float32,callback_float,"yaw_angle_tail")
+
+    rospy.Subscriber("real_input",Real_input,callback) 
     rospy.spin()
 
 # Main function 
--- reseq_ros/scripts/agevar/agevar_kinematic.py
+++ reseq_ros/scripts/agevar/agevar_kinematic.py
@@ -3,7 +3,14 @@ from math import cos, sin, sqrt
 
 # compute the linear velocity and angular velocity of the next module
 # from the values of the kinematic variables of the previous module
-def agevar_kinematic(lin_vel_in,ang_vel_in,delta,module,sign):
+
+def moduleYawRateCorrection(delta,delta_meas):
+    # Module yaw rate correction: corrects module's yaw rate (angular velocity around z, omega) taking in consideration the difference between computed and measured joint yaw angle
+    ddelta = delta_meas - delta
+    domega = ddelta/Ts
+    return domega
+
+def agevar_kinematic(lin_vel_in,ang_vel_in,delta,delta_meas,module,sign):
     if sign==1: # forward
         module=module+1
     else:       # backward
@@ -12,13 +19,14 @@ def agevar_kinematic(lin_vel_in,ang_vel_in,delta,module,sign):
     delta_dot = -(1/b)*(ang_vel_in*(b+a*cos(delta[module]))+lin_vel_in*sin(delta[module]))
     delta[module]=delta[module]+delta_dot*Ts
 
-    ang_vel_out = ang_vel_in + delta_dot
-
-    lin_vel_out_x = lin_vel_in + b*sin(delta[module])*ang_vel_out
-    lin_vel_out_y = -ang_vel_in*a - b*cos(delta[module])*ang_vel_out
+    ang_vel_out = ang_vel_in + delta_dot # theoretical module yaw rate (Omega, rotation around z)
+    domega = moduleYawRateCorrection(delta,delta_meas)
+    omega_corr = ang_vel_out + domega
+    lin_vel_out_x = lin_vel_in + b*sin(delta[module])*omega_corr
+    lin_vel_out_y = -ang_vel_in*a - b*cos(delta[module])*omega_corr
 
 
 
     lin_vel_out = sqrt(lin_vel_out_x**2 + lin_vel_out_y**2)
 
-    return lin_vel_out, ang_vel_out
+    return lin_vel_out, omega_corr
