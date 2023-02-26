03d3fbd140db4ded09361a922653acaeddd67524
Marco Fincato <marcofincato777@gmail.com>
2023-02-26 23:55:57 +0100

    Migration from CMake to pure Python package

---
+++ reseq_ros/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+package_name = 'reseq_ros'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Team ISAAC',
+    maintainer_email='team.isaac.polito@gmail.com',
+    description='The ReseQROS package',
+    license='aVeryGoodLicense!',
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
